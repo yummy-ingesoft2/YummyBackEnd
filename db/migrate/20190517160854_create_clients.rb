a2d5f5771085f7e06bd46446fd9256706bcfd264
Juan Camilo Gallego <jcgallegor@unal.edu.co>
2019-05-17 16:11:34 +0000

    primeros modelos

---
+++ db/migrate/20190517160854_create_clients.rb
@@ -0,0 +1,18 @@
+class CreateClients < ActiveRecord::Migration[5.2]
+  def change
+    create_table :clients do |t|
+      t.string :name
+      t.string :last_name
+      t.string :gender
+      t.date :birthdate
+      t.string :email
+      t.string :latitude
+      t.string :longitude
+      t.string :address
+      t.string :user
+      t.string :password
+      
+      t.timestamps
+    end
+  end
+end
